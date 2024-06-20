fdd96f293de4f73048d7d77cca5ffafb65e70a0d
danielcdz <calde0601@gmail.com>
2024-06-19 21:48:55 -0600

    Cleanup home page

--- packages/nextjs/components/Header.tsx
+++ packages/nextjs/components/Header.tsx
@@ -21,10 +21,22 @@ export const menuLinks: HeaderMenuLink[] = [
     href: "/",
   },
   {
-    label: "Debug Contracts",
+    label: "How it works",
+    href: "/",
+  },
+  {
+    label: "Get a loan",
+    href: "/",
+  },
+  {
+    label: "Give a loan",
+    href: "/",
+  },
+  {
+    label: "Debug contracts",
     href: "/debug",
     icon: <BugAntIcon className="h-4 w-4" />,
-  },
+  }
 ];
 
 export const HeaderMenuLinks = () => {
@@ -105,8 +117,7 @@ export const Header = () => {
             />
           </div>
           <div className="flex flex-col">
-            <span className="font-bold leading-tight">Scaffold-Stark</span>
-            <span className="text-xs">Starknet dev stack</span>
+            <span className="font-bold leading-tight">LendArt</span>
           </div>
         </Link>
         <ul className="hidden lg:flex lg:flex-nowrap menu menu-horizontal px-1 gap-2">
@@ -115,7 +126,6 @@ export const Header = () => {
       </div>
       <div className="navbar-end flex-grow mr-4">
         <CustomConnectButton />
-        <FaucetButton />
       </div>
     </div>
   );
